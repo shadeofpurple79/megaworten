1b76d45998a28af36a91a2f2b54750026e61c48f
shadeofpurple79 <aomoss@yahoo.com>
2022-12-20 15:07:37 +0000

    Add game rules section, create game categories dropdown list, add play and reset buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 
+// game variables
+let words = [];
+let word = "";
+let score = 0;
+let elapsedTime = 0;
+
 // create game categories
 const gameCategories = ["Fruits", "Animals"];
 
-
+// select game category
+    if (category === "Fruits") {
+        words = fruitWords;
+    } else if (category === "Animals") {
+        words = animalWords;
+    }
+  
 // create words for each fruit and map them to each fruit image
 const fruitWords = [
     {name: "der Apfel", url: "assets/images/apple.png"}
@@ -18,7 +30,7 @@ const fruitWords = [
     {name: "die Wassermelone", url: "assets/images/watermelon.png"}
 ]
 
-// create words for each fruit and map them to each animal image
+// create words for each animal and map them to each animal image
 const animalWords = [
     {name:"die Katze", url: "assets/images/cat.png""}
     {name:"der Hund", url: "assets/images/dog.png""}
@@ -30,22 +42,17 @@ const animalWords = [
     {name:"die Schildkrote", url: "assets/images/turtle.png""}
 ]
 
-// create game cards for fruits
-
-
-// create game cards for animals
-
-
 // Set the initial score and elapsed time
-score = 0;
-elapsedTime = 0;
+    score = 0;
+    elapsedTime = 0;
 
 // If the elapsed time has reached 60 seconds, end the game
     if (elapsedTime === 60) {
         endGame();
     } { (1000);
     }
-  
+ 
+// Player can reset the game and re-start the timer and score
 
 
 
