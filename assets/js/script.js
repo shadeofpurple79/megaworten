cafe9fbc7e232ff544720b788023c3e055bfc122
shadeofpurple79 <aomoss@yahoo.com>
2022-12-23 20:38:23 +0000

    Add boilerplate javascript code for each step of the game, create game consts and variables to link html and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,109 @@
-const title = document.getElementById ('game-controls')
-const title = document.getElementById ('category-dropdown')
-const title = document.getElementById ('dropdown-content')
-const title = document.getElementById ('play-button')
-const title = document.getElementById ('game-cards')
-const title = document.getElementById ('game-answers')
-const title = document.getElementById ('player-answers')
+const gameControls = document.getElementById ('game-controls')
+const categoryDropdown = document.getElementById ('category-dropdown')
+const dropdownContent = document.getElementById ('dropdown-content')
+const gameCards = document.getElementById ('game-cards')
+const gameAnswers = document.getElementById ('game-answers')
+const playerAnswers = document.getElementById ('player-answers')
+const playButton = document.getElementById ('play-button')
+const resetButton = document.getElementById ('reset-button')
 
+// game variables
+let = play
+let = reset
+let words = [];
+let word = "";
 
-// On pageload, or on reset buttonclick, or play-again buttonclick, show game category selection dropdown
+// game categories
+const gameCategories = ["Fruits", "Animals"];
+
+// create words for each fruit and map them to each fruit image
+const fruitWords = [
+  {name: "der Apfel", img: "assets/images/apple.png",}
+  {name: "die Banana", img: "assets/images/banana.png",}
+  {name: "die Zitrone", img: "assets/images/lemon.png",}
+  {name: "die Orange", img: "assets/images/orange.png",}
+  {name: "der Pfirsich", img: "assets/images/peach.png"},
+  {name: "die Birne", img: "assets/images/pear.png"},
+  {name: "die Ananas", img: "assets/images/pineapple.png"},
+  {name: "die Pflaume", img: "assets/images/plum.png"},
+  {name: "der Granatapfel", img: "assets/images/pomegranet.png"},
+  {name: "die Erdbeere", img: "assets/images/strawberry.png"},
+  {name: "die Wassermelone", img: "assets/images/watermelon.png"},
+]
+
+// create words for each animal and map them to each animal image
+const animalWords = [
+  {name:"die Katze", img: "assets/images/cat.png"},
+  {name:"der Hund", img: "assets/images/dog.png"},
+  {name:"der Goldfisch", img: "assets/images/goldfish.png"},
+  {name:"die Maus", img: "assets/images/mouse.png"},
+  {name:"der Wellensittich", img: "assets/images/parrot.png"},
+  {name:"das Kaninchen", img: "assets/images/rabbit.png"},
+  {name:"die Schlange", img: "assets/images/snake.png"},
+  {name:"die Schildkrote", img: "assets/images/turtle.png"},
+]
+
+// On pageload, show game category selection dropdown
+window.onload = function() {
+  document.getElementById("dropdown-content").style.display = "block";
+}
 
 // Listen for game category selection
+ if (dropdownContent === "Fruits") {
+  words = fruitWords;
+  numberOfCardsInSelectedCategory = fruitWords.length
+} else if (dropdownContent === "Animals") {
+  words = animalWords;
+  numberOfCardsInSelectedCategory = animalWords.length
+}
+document.getElementById(words).style.display = "block";
 
-// Show PLAY button
+// Show PLAY button, ---Hide the dropdown menu
+function showPlay() {
+  document.getElementById("play-button").style.display = "block";
+}
 
-// Listen for PLAY button click
+// On reset buttonclick, show game category selection dropdown
+resetButton.addEventListener("click", () => {
+  document.getElementById("dropdown-content").style.display = "block";
+})
+
+// On play-again buttonclick, show game category selection dropdown
+playButton.addEventListener("click", () => {
+  document.getElementById("dropdown-content").style.display = "block";
+})
 
 // Reset timer, Reset score
     let score = 0;
     let timer = 0;
+    // Start countdown
+    var count = 60, timer = setInterval(function() {
+      $("#counter").html(count--);
+      if(count == 1) clearInterval(timer);
+  }, 1000);
 
-// Start countdown timer
-
-// Throughout the game, Listen for RESET button click, back to first step when clicked
 
 // Show cards count in the selected game category
 
-// Show a random card from selected game category
+// for 60 seconds
+     
+  // Show a random card from selected game category
 
-// Listen for player answer
+  // Listen for player answer
 
-// Check answer
+  // Check answer
 
-  // Wrong answer == > go back to Show a random card from selected game category
+    // Wrong answer == > go back to Show a random card from selected game category
 
-  // Correct answer == > Remove card from game, increase score by 1, decrease card count in the selected game category by 1
+    // Correct answer == > Remove card from game, increase score by 1, decrease card count in the selected game category by 1
 
 // check elapsed time
-
+  function timer() {
+    if (elapsedTime === 60) {
+      endGame()
+    }
+    1000;;
+  }
   // timer NOT zero, card count NOT zero == > back to Show a random card from selected game category
 
   // timer zero, card count NOT zero == > Game over, you lost, play again?
@@ -49,51 +116,4 @@ const title = document.getElementById ('player-answers')
 
 
 
-// game variables
-let = play
-let = reset
-let words = [];
-let word = "";
 
-
-
-// game categories
-const gameCategories = ["Fruits", "Animals"];
-
-// game cards
-  
-// create words for each fruit and map them to each fruit image
-const fruitWords = [
-    {name: "der Apfel", img: "assets/images/apple.png",}
-    {name: "die Banana", img: "assets/images/banana.png",}
-    {name: "die Zitrone", img: "assets/images/lemon.png",}
-    {name: "die Orange", img: "assets/images/orange.png",}
-    {name: "der Pfirsich", img: "assets/images/peach.png"},
-    {name: "die Birne", img: "assets/images/pear.png"},
-    {name: "die Ananas", img: "assets/images/pineapple.png"},
-    {name: "die Pflaume", img: "assets/images/plum.png"},
-    {name: "der Granatapfel", img: "assets/images/pomegranet.png"},
-    {name: "die Erdbeere", img: "assets/images/strawberry.png"},
-    {name: "die Wassermelone", img: "assets/images/watermelon.png"},
-]
-
-// create words for each animal and map them to each animal image
-const animalWords = [
-    {name:"die Katze", img: "assets/images/cat.png"},
-    {name:"der Hund", img: "assets/images/dog.png"},
-    {name:"der Goldfisch", img: "assets/images/goldfish.png"},
-    {name:"die Maus", img: "assets/images/mouse.png"},
-    {name:"der Wellensittich", img: "assets/images/parrot.png"},
-    {name:"das Kaninchen", img: "assets/images/rabbit.png"},
-    {name:"die Schlange", img: "assets/images/snake.png"},
-    {name:"die Schildkrote", img: "assets/images/turtle.png"},
-]
-
-// select game category
-if (gameCategories === "Fruits") {
-    words = fruitWords;
-    numberOfCardsInSelectedCategory = fruitWords.length
-} else if (gameCategories === "Animals") {
-    words = animalWords;
-    numberOfCardsInSelectedCategory = animalWords.length
-}
