53a17f8d58d09a965f1a974f03ef3536ad7966c9
shadeofpurple79 <aomoss@yahoo.com>
2023-03-16 10:01:17 +0000

    Update var to let on scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 
 // Game rules open/close modal 
 // Get the modal
-var modal = document.getElementById("rulesModal");
+let modal = document.getElementById("rulesModal");
 // Get the button that opens the modal
-var btn = document.getElementById("rulesBtn");
+let btn = document.getElementById("rulesBtn");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
